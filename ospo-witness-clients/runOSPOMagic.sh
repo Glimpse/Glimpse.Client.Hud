a6acb9600a18c41a7193653e4113dbd5c248cc99
Mike Kaufman <mkaufman@microsoft.com>
2017-05-04 09:45:30 -0700

    fixup bash script

--- ospo-witness-clients/runOSPOMagic.sh
+++ ospo-witness-clients/runOSPOMagic.sh
@@ -13,15 +13,15 @@ echo "generating oscg-cart-overrides..."
 node $DIR/generateOsscgCartOverrides.js $projectDir/package.json > $projectDir/osscg-cart-overrides.json
 cd $DIR
 
-if [ -d  $DIR/ospo-witness-clients]; then
+if [ ! -d  $DIR/ospo-witness-clients ]; then
     echo "cloning ospo-witness-clients repo..."
     git clone https://github.com/Microsoft/ospo-witness-clients.git
 else
     echo "echo ospo-witness-clients repo already exists.  Not cloning."
 fi
 
-cd ospo-witness-clients/npm
-call npm install
+cd $DIR/ospo-witness-clients/npm
+npm install
 echo "running ospo-witness-clients command..."
 node index.js register -i $projectDir -o ./ospo.out --url https://witness.azurewebsites.net/ -t Iwfr7h4amgd2rdk2755iqjykm2ovcqm5h2an5ohopa7ak5fmla3q
 popd
