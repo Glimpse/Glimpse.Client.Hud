037e29f2e9b6ceadc77a421e7cc832f0bb4a80cc
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2017-06-08 09:27:17 -0700

    Update to split out timing data into its own proxy

--- src/views/timing.js
+++ src/views/timing.js
@@ -1,34 +1,8 @@
-const timingsRaw = (window.performance ||
-    window.mozPerformanceperformance ||
-    window.msPerformanceperformance ||
-    window.webkitPerformanceperformance ||
-    {}).timing;
-let timingIncomplete = false;
-
-function calculateTimings(timingsRaw, startIndex, finishIndex) {
-  // avoid negative values
-  return Math.max(0, timingsRaw[finishIndex] - timingsRaw[startIndex]);
-}
-
-function getTimings(timingsRaw) {
-    let network =
-        calculateTimings(timingsRaw, 'responseStart', 'responseEnd') +
-        calculateTimings(timingsRaw, 'navigationStart', 'requestStart');
-    let server = calculateTimings(timingsRaw, 'requestStart', 'responseEnd');
-    let browser = calculateTimings(timingsRaw, 'responseStart', 'loadEventEnd');
-    let total = calculateTimings(timingsRaw, 'navigationStart', 'loadEventEnd');
-
-    return {
-        network,
-        server,
-        browser,
-        total
-    };
-}
+const timingProxy = require('../proxy/timing');
 
 module.exports = {
     render: function() {
-        const timings = getTimings(timingsRaw);
+        const timings = timingProxy.getTimings();
 
         return `
             <div class="glimpse-section glimpse-section--first glimpse-timing">
