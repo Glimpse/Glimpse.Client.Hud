a3a61d8cee25cd5f891b7e009d3ac7b7abf1d26b
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2017-06-08 11:30:23 -0700

    Initial work to introduce popups to sections

--- src/views/timing.js
+++ src/views/timing.js
@@ -6,15 +6,25 @@ module.exports = {
 
         return `
             <div class="glimpse-section glimpse-section--first glimpse-timing">
-                <span class="glimpse-section-label">
-                    Page load time
-                </span>
-                <span class="glimpse-section-duration glimpse-section-value">
-                    ${timings.total}
-                </span>
-                <span class="glimpse-section-suffix">
-                    ms
-                </span>
+                <div class="glimpse-section-summary">
+                    <span class="glimpse-section-label">
+                        Page load time
+                    </span>
+                    <span class="glimpse-section-duration glimpse-section-value">
+                        ${timings.total}
+                    </span>
+                    <span class="glimpse-section-suffix">
+                        ms
+                    </span>
+                </div>
+                <div class="glimpse-section-detail">
+                    test<br />
+                    test<br />
+                    test<br />
+                    test<br />
+                    test<br />
+                    test
+                </div>
             </div>
         `;
     }
