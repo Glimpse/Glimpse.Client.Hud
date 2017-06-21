20180b4ed4ee979ae714b30acb5651428374a861
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2017-06-21 06:58:47 -0700

    Adds functionality which adds data functionality

--- src/index.js
+++ src/index.js
@@ -10,16 +10,19 @@ var versionView = require('./views/version');
 var openView = require('./views/open');
 var timingView = require('./views/timing');
 var ajaxView = require('./views/ajax');
+var dataView = require('./views/data');
 
-function render() {
+function render(initPromise) {
     return `
         <div class="glimpse-hud">
             <div class="glimpse-hud-data">
                 ${versionView.render()}
                 ${timingView.render()}
+                ${dataView.render()}
                 ${ajaxView.render()}
                 <div class="glimpse-hud-popup">
                     ${timingView.renderPopup()}
+                    ${dataView.renderPopup()}
                     ${ajaxView.renderPopup()}
                 </div>
             </div>
@@ -28,22 +31,32 @@ function render() {
     `;
 }
 
-function postRender() {
-    ajaxView.postRender();
+function postRender(initPromise) {
+    ajaxView.postRender(initPromise);
 }
 
-let timeout = 1;
-const onTimeout = () => {
-    if (document.readyState === 'complete') {
-        const container = document.createElement('div');
-        container.innerHTML = render();
-        document.body.appendChild(container);
-
-        postRender();
-    }
-    else {
-        setTimeout(onTimeout, timeout *= 2);
-    }
+function preInit(initPromise) {
+    dataView.preInit(initPromise);
 }
 
-setTimeout(onTimeout);
+const init = new Promise(function(resolve, reject) {
+    preInit(init);
+
+    let timeout = 1;
+    const onTimeout = () => {
+        if (document.readyState === 'complete') {
+            const container = document.createElement('div');
+            container.innerHTML = render(init);
+            document.body.appendChild(container);
+
+            postRender(init);
+
+            resolve(container);
+        }
+        else {
+            setTimeout(onTimeout, timeout *= 2);
+        }
+    }
+
+    setTimeout(onTimeout);
+});
---
+++ src/views/data.js
@@ -0,0 +1,99 @@
+const util = require('../lib/util');
+const messagesUtil = require('../lib/messages');
+
+const defaultSummary = { total: '--', time: '--' }
+
+function processType(type, indexedMessages) {
+    const messages = indexedMessages[type];
+    const result = { total: 0, time: 0 };
+    if (messages && messages.length) {
+        messages.forEach(function(message) {
+            result.total++;
+            result.time = parseInt(message.payload.duration);
+        });
+    }
+    return result;
+}
+function process(indexedMessages) {
+    const result = {};
+    result.httpClient = processType('data-http-response', indexedMessages);
+    result.dataStore = processType('data-store-end', indexedMessages);
+    result.summary = {
+        total: result.httpClient.total + result.dataStore.total,
+        time: result.httpClient.time + result.dataStore.time
+    };
+    return result;
+}
+
+function update(data) {
+    updateValue('glimpse-data-summary-value', data.summary);
+    updateValue('glimpse-data-popup-summary-value', data.summary);
+    updateValue('glimpse-data-popup-httpClient-value', data.httpClient);
+    updateValue('glimpse-data-popup-dataStore-value', data.dataStore);
+}
+function updateValue(target, data) {
+    document.getElementById(target).innerHTML = data.total + ' / ' + data.time;
+}
+
+module.exports = {
+    preInit: function(initPromise) {
+        const contextMessagesPromise = fetch(util.resolveContextUrl(util.currentRequestId()))
+            .then(function(response) {
+                return response.json();
+            });
+
+        Promise.all([
+            contextMessagesPromise,
+            initPromise
+        ]).then(values => {
+            const rawMessages = values[0];
+            const indexedMessages = messagesUtil.indexMessages(rawMessages);
+            const data = process(indexedMessages);
+            update(data);
+        });
+    },
+    render: function() {
+        const timings = { summary: defaultSummary };
+
+        return `
+            <div class="glimpse-section glimpse-data">
+                <div class="glimpse-section-summary">
+                    <div class="glimpse-hud-field">
+                        <div class="glimpse-hud-field-label">
+                            I/O Calls
+                        </div>
+                        <div class="glimpse-hud-field-value glimpse-time-ms" id="glimpse-data-summary-value">
+                            ${timings.summary.total} / ${timings.summary.time}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        `;
+    },
+    renderPopup: function() {
+        const timings = { summary: defaultSummary, httpClient: defaultSummary, dataStore: defaultSummary };
+
+        return `
+            <div class="glimpse-hud-popup-section">
+                <div class="glimpse-hud-field">
+                    <div class="glimpse-hud-field-label">I/O Calls</div>
+                    <div class="glimpse-hud-field-value glimpse-time-ms" id="glimpse-data-popup-summary-value">
+                        ${timings.summary.total} / ${timings.summary.time}
+                    </div>
+                    <div class="glimpse-hud-field">
+                        <div class="glimpse-hud-field-label">Http Client</div>
+                        <div class="glimpse-hud-field-value glimpse-time-ms" id="glimpse-data-popup-httpClient-value">
+                            ${timings.httpClient.total} / ${timings.httpClient.time}
+                        </div>
+                    </div>
+                    <div class="glimpse-hud-field">
+                        <div class="glimpse-hud-field-label">Data Store</div>
+                        <div class="glimpse-hud-field-value glimpse-time-ms" id="glimpse-data-popup-dataStore-value">
+                            ${timings.dataStore.total} / ${timings.dataStore.time}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        `;
+    }
+};
