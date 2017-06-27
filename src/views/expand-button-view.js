5a4d0d746a0cdbb077c54258dd849032492e3b23
legomushroom <legomushroom@gmail.com>
2017-06-27 14:55:31 -0500

    [expand button]: change expand icon

--- src/views/expand-button-view.js
+++ src/views/expand-button-view.js
@@ -1,13 +1,17 @@
+const icons = require('../assets/icons').default;
+
 module.exports = {
     render: function() {
         return `
             <div class="glimpse-section glimpse-expand-button" id="js-glimpse-expand-button" title="expand">
+              ${icons.expandArrow}
             </div>
         `;
     },
     renderPopup: function() {
         return `
-            <div class="glimpse-hud-popup-section glimpse-hud-popup-section--arrow" id="js-glimpse-collapse-button" title="collapse">
+            <div class="glimpse-hud-popup-section glimpse-hud-popup-section--expandArrow" id="js-glimpse-collapse-button" title="collapse">
+              ${icons.expandArrow}
             </div>
         `;
     }
