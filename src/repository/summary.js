806812fa6a33b6879a66a83dd8e00ed2b20998c0
David Khourshid <davidkpiano@gmail.com>
2017-06-30 14:35:15 -0700

    Substituting Fetch with XHR (#162)

--- src/repository/summary.js
+++ src/repository/summary.js
@@ -1,11 +1,19 @@
 const util = require('../lib/util');
 
-const getPromise = fetch(util.resolveContextSummaryUrl(util.currentRequestId()))
-    .then(
-        function(response) {
-            return response.json();
-        }
-    );
+const getPromise = new Promise(function(resolve, reject) {
+    const request = new XMLHttpRequest();
+
+    request.onload = function() {
+        resolve(JSON.parse(this.responseText));
+    }
+    
+    request.onerror = function(err) {
+        reject(err);
+    }
+
+    request.open('get', util.resolveContextSummaryUrl(util.currentRequestId()), true);
+    request.send(); 
+});
 
 module.exports = {
     getPromise: function() {
