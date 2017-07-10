a7c4a64c68bb5160034a1b31bcc2e62667f95122
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2017-07-10 16:49:23 -0700

    Fix fake data generator given switch from fetch to xhr

--- src/repository/summary.js
+++ src/repository/summary.js
@@ -4,15 +4,17 @@ const getPromise = new Promise(function(resolve, reject) {
     const request = new XMLHttpRequest();
 
     request.onload = function() {
-        resolve(JSON.parse(this.responseText));
+        const response = FAKE_SERVER ? this.fakeResponseText : this.responseText;
+
+        resolve(JSON.parse(response));
     }
-    
+
     request.onerror = function(err) {
         reject(err);
     }
 
     request.open('get', util.resolveContextSummaryUrl(util.currentRequestId()), true);
-    request.send(); 
+    request.send();
 });
 
 module.exports = {
